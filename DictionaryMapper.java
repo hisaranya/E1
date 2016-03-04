f5026e265a08679f3b921878c69efa31c1a05c7b
hisaranya <hisaranya@gmail.com>
2016-03-04 13:26:24 -0800

    sample run

--- DictionaryMapper.java
+++ DictionaryMapper.java
@@ -8,14 +8,33 @@ import org.apache.hadoop.mapreduce.lib.input.FileSplit;
 
 public class DictionaryMapper  extends Mapper<Text, Text, Text, Text> {
       // TODO define class variables for translation, language, and file name
+      private Text word = new Text();
+
 
       public void setup(Context context) {    	   
       // TODO determine the language of the current file by looking at it's name
+
+      	//FileSplit fileSplit = (FileSplit)context.getInputSplit();
+        //String filename = fileSplit.getPath().getName();
+
       }
 
       public void map(Text key, Text value, Context context) throws IOException, InterruptedException {
       // TODO instantiate a tokenizer based on a regex for the file
 
+  StringTokenizer itr = new StringTokenizer(value.toString(),",");
+            while (itr.hasMoreTokens())
+            {
+                word.set(itr.nextToken());
+                context.write(key, word);
+            }
+        }      
+
+//if (line.trim().startsWith("#"))
+	//{
+	//    continue; // continue while() loop
+   // }
+
       // iterate through the tokens of the line, parse, transform and write to context
       }
-}
+
--- DictionaryReducer.java
+++ DictionaryReducer.java
@@ -5,7 +5,20 @@ import org.apache.hadoop.io.Text;
 import org.apache.hadoop.mapreduce.Reducer;
 
 public class DictionaryReducer extends Reducer<Text,Text,Text,Text> {
+
+private Text result = new Text();
+
+
    public void reduce(Text word, Iterable<Text> values, Context context ) throws IOException, InterruptedException {
       // TODO iterate through values, parse, transform, and write to context
+
+
+   	 String translations = "";
+            for (Text val : values)
+            {
+                translations += "|"+val.toString();
    }
+    result.set(translations);
+            context.write(key, result);
+}
 }
